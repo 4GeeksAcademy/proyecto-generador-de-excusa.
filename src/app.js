d1ceda75296e8410bd63f1fc175159806e16753c
sebastian-solorzano95 <sebassolorzano95@gmail.com>
2025-06-13 01:46:40 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -8,4 +8,12 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+let who = ['la lora', 'la cucaracha', 'el drilococo', 'la sarigueya'];
+let action = ['cotorreaba', 'se revolcaba', 'tomaba el sol', 'se electocutaba'];
+let what = ['en casa', 'en el piso', 'en el rio', 'por accidente'];
+let when = ['cuando hay compañia', 'cuando la fumigaron', '', 'para calentarse', 'sobre una fuente electrica'];
+
+const pickRandomElement = (array) => array[Math.floor(Math.random() * array.length)]
+let excusa = [pickRandomElement(who)] + " " + [pickRandomElement(action)] + " " + [pickRandomElement(what)] + " " + [pickRandomElement(when)];
+document.getElementById("excusas-random").textContent = excusa;
 };
